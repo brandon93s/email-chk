7db400432d4240fbf9ed7817119b858349141430
Brandon Smith <brandon93s@gmail.com>
2017-09-17 17:13:29 -0400

    Implement email-chk

--- test/index.js
+++ test/index.js
@@ -1,1 +1,22 @@
 import test from 'ava'
+import emailChk from '../index'
+
+test('Non-email returns false', async t => {
+  const result = await emailChk('not-an-email')
+  t.false(result)
+})
+
+test('Valid email returns true', async t => {
+  const result = await emailChk('noreply@github.com')
+  t.true(result)
+})
+
+test('Invalid email returns false', async t => {
+  const result = await emailChk('test@example.com')
+  t.false(result)
+})
+
+test('Invalid domain returns false', async t => {
+  const result = await emailChk('test@example.invaliddns')
+  t.false(result)
+})
